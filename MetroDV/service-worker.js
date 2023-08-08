9cc96030db1b6ca7edb6849f505c364606404958
badgate503 <badgate503@users.noreply.github.com>
2023-08-08 12:27:54 +0000

    deploy: 8051f6fafa3a5ea13ca1273fa96d72b9902f1a35

--- MetroDV/service-worker.js
+++ MetroDV/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de70a511dcd30abb6dd2586cec0f3bc8.js"
+  "./precache-manifest.3bf30044b533c3827012ef59f724b24b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
